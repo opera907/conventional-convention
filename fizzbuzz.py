ea7545610c12be552069320ec72d44b0e1bdbd26
dahee <opera9070@gmail.com>
2021-11-23 17:13:22 +0900

    feat: Print 'buzz' When i gose to 5n of i
    printing buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 for (let i=1; i <= 16 ; i++){
     if(i%3 === 0){
             console.log('fizz')
+        } if(i%5 === 0){
+            console.log('buzz')
         } else{
             console.log(i)
         }
