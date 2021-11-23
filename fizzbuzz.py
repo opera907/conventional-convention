38a45f36ac992fa749c6ecc5b9a1df87a75ffb03
dahee <opera9070@gmail.com>
2021-11-23 17:23:28 +0900

    feat: Pring 'fizzbuzz' when i gose to times of 15
    Complete.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,10 @@
 for (let i=1; i <= 16 ; i++){
-    if(i%3 === 0){
+        if(i%15 === 0){
+            console.log('fizzbuzz')
+        }
+        else if(i%3 === 0){
             console.log('fizz')
-        } if(i%5 === 0){
+        } else if(i%5 === 0){
             console.log('buzz')
         } else{
             console.log(i)
