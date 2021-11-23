0019d93ce68ab6312a2b967efb62944f74bd6f64
dahee <opera9070@gmail.com>
2021-11-23 17:33:58 +0900

    feat: trailing comma test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
-fruits = ['Apple', 'Banana', 'Pineapple',]
+fruits = ['Apple', 'Banana', 'Pineapple', 3]
 
 animals = [
         'Lion',
         'Tiger',
         'Rabbit',
+        3
         ]
 
 clubs = [
     'ManU',
     'ManC',
+    3,
 ]
 
 for (let i=1; i <= 16 ; i++){
